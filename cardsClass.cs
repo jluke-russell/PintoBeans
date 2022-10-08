56e54de7a03f7fab3125559a67cc6fd894df291f
Nerdbisque <113635080+Nerdbisque@users.noreply.github.com>
2022-10-07 19:51:49 -0600

    Final no COmments

--- cardsClass.cs
+++ cardsClass.cs
@@ -1,19 +1,15 @@
 using System;
 
 namespace cse210_student_csharp_Hi_Lo
-{   
-    class cardClass
 {
     class Card
     {
         public int cardValue;
 
         ///Create a class for a card/draws a card
-        public void getNewCard()
-            {
-                Random randomGenerator = new Random();
-                cardValue = randomGenerator.Next(1,14);
-            }
+        public void getNewCard(){
+            Random randomGenerator = new Random();
+            cardValue = randomGenerator.Next(1,14);
         }
     }
 }
